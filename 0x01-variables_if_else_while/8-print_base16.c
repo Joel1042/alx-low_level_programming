400916ef9fb680db0b60ce929c1b83db7b806343
joel1042 <joelwilson453@gmail.com>
2022-10-30 09:37:46 -0700

    print base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int num = '0';
+
+	char letter = 'a';
+
+	while  (num <= '9')
+	{
+		putchar(num);
+		num++;
+	}
+	while (letter <= 'f')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar ('\n');
+	return (0);
+}
