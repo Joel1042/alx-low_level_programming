f54e8a67951843612457a4d1c02bb5e35ab55fbb
joel1042 <joelwilson453@gmail.com>
2022-10-29 22:23:42 -0700

    print alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char x = 'a';
+	char X = 'A';
+
+	while (x <= 'a')
+	{
+		putchar(x);
+		x++;
+	}
+	while (X <= 'A')
+	{
+		putchar(X);
+		X++;
+	}
+
+	putchar('\n');
+	return (0);
+}
