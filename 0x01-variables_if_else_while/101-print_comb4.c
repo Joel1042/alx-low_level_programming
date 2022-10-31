ab4dff2d4bca6b795604747453ade517dd65f426
joel1042 <joelwilson453@gmail.com>
2022-10-31 12:48:08 -0700

    print combo 4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int x = '0';
+	int y = '0';
+	int z = '0';
+
+	for (z = '0'; z <= '9'; z++)
+	{
+		for (y = '0'; y <= '9'; y++)
+		{
+			for (x = '0'; x <= '9'; z++)
+			{
+				if (!((x == y) || (y == z) || (y > x) || (z > y)))
+				{
+					putchar(z);
+					putchar(y);
+					putchar(x);
+					if (!(x == '9' && y == '8' && z == '7'))
+					{
+						putchar(',');
+						putchar (' ');
+					}
+				}
+			}
+		}
+	}
+
+	putchar ('\n');
+	return (0);
+}
+
