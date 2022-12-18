7554c97acd104244ac561ac2276beaf3235fc48b
joel1042 <joelwilson453@gmail.com>
2022-12-17 22:54:31 -0800

    jack_bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * jack_bauer -prints every minute of the day
+ */
+
+void jack_bauer(void)
+{
+	int hour0;
+	int hour1;
+	int minute0;
+	int minute1;
+
+	for (hour0 = 0; hour0 <= 2; hour0++)
+	{
+		for (hour1 = 0; hour1 < 4; hour1++)
+		{
+			for (minute0 = 0; minute0 <= 5; minute0++)
+			{
+				for (minute1 = 0; minute0 <= 9; minute1++)
+				{
+					_putchar('0' + hour0);
+					_putchar('0' + hour1);
+					_putchar(';');
+					_putchar('0' + minute0);
+					_putchar('0' + minute1);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
