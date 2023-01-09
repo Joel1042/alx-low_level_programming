aaa4434ec91b55a9af0fce5fdad90400afd3c6ce
joel1042 <joelwilson453@gmail.com>
2023-01-09 01:42:04 -0800

    print prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * is_prime_number - prints prime number
+ * @n: the prime number
+ * Return: 1 if prime number,0 if not
+ */
+int is_prime_number(int n)
+{
+	int x = 0;
+	int y = 0;
+
+	if (x * y == n || x > y)
+		return (1);
+
+	if (x * y ==n || x == y)
+
+		return (0);
+	return (is_prime_number(n));
+}
